74917a698a48b46872e0fda4868e4a109fcc5c79
sanchit2843 <sanchittanwar75@gmail.com>
2018-10-04 15:34:34 +0530

    Requirment file added

--- gettingdata.py
+++ gettingdata.py
@@ -2,6 +2,7 @@ import hdf5storage
 import datetime
 from datetime import date
 import numpy as np
+
 mat = hdf5storage.loadmat('wiki.mat')
 wiki = mat['wiki']
 gender = wiki['gender']
@@ -12,7 +13,28 @@ dob = wiki['dob']
 dob = dob[0,0]
 path = wiki['full_path']
 path = path[0,0]
+
+def create_csv(file,name):
+    a = pd.DataFrame(file)
+    a.to_csv(name)
+def datenum_to_datetime(datenum):
+    """
+    Convert Matlab datenum into Python datetime.
+    :param datenum: Date in datenum format
+    :return:        Datetime object corresponding to datenum.
+    """
+    days = datenum % 1
+    hours = days % 1 * 24
+    minutes = hours % 1 * 60
+    seconds = minutes % 1 * 60
+    return date.fromordinal(int(datenum)) \
+           + timedelta(days=int(days)) \
+           + timedelta(hours=int(hours)) \
+           + timedelta(minutes=int(minutes)) \
+           + timedelta(seconds=round(seconds)) \
+           - timedelta(days=366)
 #reshaping arrays rowwise
+
 dob = np.reshape(dob,(62328,1))
 gender = np.reshape(gender,(62328,1))
 path = np.reshape(path,(62328,1))
@@ -33,22 +55,3 @@ create_csv(dob,name = 'dob.csv')
 create_csv(gender,name = 'gender.csv')
 create_csv(path,name = 'path.csv')
 create_csv(photo_taken,name = 'photo_taken.csv')
-def create_csv(file,name):
-    a = pd.DataFrame(file)
-    a.to_csv(name)
-def datenum_to_datetime(datenum):
-    """
-    Convert Matlab datenum into Python datetime.
-    :param datenum: Date in datenum format
-    :return:        Datetime object corresponding to datenum.
-    """
-    days = datenum % 1
-    hours = days % 1 * 24
-    minutes = hours % 1 * 60
-    seconds = minutes % 1 * 60
-    return date.fromordinal(int(datenum)) \
-           + timedelta(days=int(days)) \
-           + timedelta(hours=int(hours)) \
-           + timedelta(minutes=int(minutes)) \
-           + timedelta(seconds=round(seconds)) \
-           - timedelta(days=366)
