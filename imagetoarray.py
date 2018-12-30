789d9fe273a41e3ed823e6db4fd6b924b8649522
sanchit2843 <sanchittanwar75@gmail.com>
2018-12-30 18:36:25 +0530

    label creation code added

--- imagetoarray.py
+++ imagetoarray.py
@@ -12,16 +12,11 @@ class images_to_array():
         self.agedirectory = agedirec
     def extract(self):
         a = os.listdir(self.directory)
-        labels = pd.read_csv(self.agedirectory)
-        age = labels.iloc[:,1:2].values
-
-        ohe = OneHotEncoder(categorical_features = [0])
-        age = ohe.fit_transform(age).toarray()
-        d = 3*self.imagesize
-        image = np.ones((23708,d,self.imagesize),int)
+        image = np.ones((23708,self.imagesize,self.imagesize),int)
         image = np.reshape(image,(23708,self.imagesize,self.imagesize,3))
         for i in tqdm(range(len(a))):
           path = os.path.join(self.directory,str(a[i]))
           image1 = cv2.imread(path)
           image1 = cv2.resize(image1,(self.imagesize,self.imagesize))
           image[i,:,:,:] = image1
+        return image
